661fffe73eb0d678cdd9e7544919ed87ca51a837
Satyarth Toshniwal <147167270+satyapro555@users.noreply.github.com>
2024-04-24 17:44:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+var type = new Typed('.textline ',{
+    strings:["TEXT ANIMATIONS","MOTION GRAPHICS"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop: true
+})
+
+function myFunc() { 
+    window.location.href = "./login/login.html"; 
+}
